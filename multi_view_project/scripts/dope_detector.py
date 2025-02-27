5a62ff583e9e7f0222f8909d25a30cef2faeabde
TomCJN <tommynicholls101@gmail.com>
2025-02-27 13:13:10 +0000

    Added transformations to dope detector

--- multi_view_project/scripts/dope_detector.py
+++ multi_view_project/scripts/dope_detector.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
 import rospy
+import tf
 from vision_msgs.msg import Detection3DArray
 
 def callback(data):
     rospy.loginfo("Number of detected objects: %d", len(data.detections))
     rospy.loginfo("Total Objects: %d", len(data.detections))
+    
+    tf_listener = tf.TransformListener()
 
+    try:
+        tf_listener.waitForTransform("/world", "/camera_link", rospy.Time(0), rospy.Duration(1.0))
+        (trans, rot) = tf_listener.lookupTransform("/world", "/camera_link", rospy.Time(0))
+
+        rospy.loginfo(f"World to Camera Transform: Translation = {trans}, Rotation = {rot}")
+
+    except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+        rospy.logwarn("Could not get world to camera transform")
+        return
+    
+    
     for i, detection in enumerate(data.detections):
         obj_id = detection.results[0].id
         confidence_score = detection.results[0].score
@@ -17,6 +31,13 @@ def callback(data):
         rospy.loginfo(f"  - Confidence Score: {confidence_score:.3f}")
         rospy.loginfo(f"  - Position: x={position.x:.3f}, y={position.y:.3f}, z={position.z:.3f}")
         rospy.loginfo(f"  - Orientation: x={orientation.x:.3f}, y={orientation.y:.3f}, z={orientation.z:.3f}, w={orientation.w:.3f}")
+        
+        obj_trans = (position.x, position.y, position.z)
+        obj_rot = (orientation.x, orientation.y, orientation.z, orientation.w)
+
+        obj_in_world = tf_listener.fromTranslationRotation(obj_trans, obj_rot)
+
+        rospy.loginfo(f"Object {i+1} in World Frame: {obj_in_world}")
 
 
 def dope_detector():
